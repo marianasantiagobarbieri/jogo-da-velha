79a63793accd95934e3afbf4422779583da6be27
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-28 22:54:47 -0300

    arrumando bug

--- pwa-jogo-da-velha/js/main.js
+++ pwa-jogo-da-velha/js/main.js
@@ -6,21 +6,21 @@ window.onload = () => {
 }
 
 
-const QuadradoJogos = document.querySelectorAll(".QuadradoJogo");
+const linhaJogos = document.querySelectorAll(".linhaJogo");
 let checarVezJogador = true;
 
 const jogadorX = "X";
 const jogadorO = "O";
 
 document.addEventListener("click", (event) => {
-    if(event.target.matches(".QuadradoJogo")) {
+    if(event.target.matches(".linhaJogo")) {
         jogar(event.target.id);
     }
 }); 
 
 function jogar(id) {
-    const QuadradoJogo = document.getElementById(id);
+    const linhaJogo = document.getElementById(id);
     vezDoJogador = checarVezJogador ? jogadorX : jogadorO;
-    QuadradoJogo.textContent = vezDoJogador;
+    linhaJogo.textContent = vezDoJogador;
     checarVezJogador = !checarVezJogador;
 }
