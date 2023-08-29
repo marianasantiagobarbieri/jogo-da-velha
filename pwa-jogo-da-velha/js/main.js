ff8c43bc88526df343016967286b04a05fc365d0
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-29 10:49:28 -0300

    fazendo as funções do jogo da velha no javascript

--- pwa-jogo-da-velha/js/main.js
+++ pwa-jogo-da-velha/js/main.js
@@ -12,6 +12,17 @@ let checarVezJogador = true;
 const jogadorX = "X";
 const jogadorO = "O";
 
+const chancesVencedor = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]; /* quanridade de combinações possíveis para ganhar o jogo  */
+
 document.addEventListener("click", (event) => {
     if(event.target.matches(".linhaJogo")) {
         jogar(event.target.id);
@@ -22,5 +33,15 @@ function jogar(id) {
     const linhaJogo = document.getElementById(id);
     vezDoJogador = checarVezJogador ? jogadorX : jogadorO;
     linhaJogo.textContent = vezDoJogador;
+    linhaJogo.classList.add(vezDoJogador); 
     checarVezJogador = !checarVezJogador;
+    verificarVencedor(vezDoJogador);
 }
+
+function verificarVencedor(vezDoJogador) {
+    const vencedor = chancesVencedor.some((combinacao) =>{
+        return combinacao.every((index) => {
+            return linhaJogos[index].classList.contains(vezDoJogador);
+        })
+    });/* início da validação se haverá ou não um vencedor, pois será percorrido o array chancesVencedor e se algum for true já teremos um vencedor a caminho*/
+}/* o some vai ajudar a percoorer meu array chancesVencedor e se der true el alguma combinação o array inteiro vai estar certo */
