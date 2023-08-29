5f34e9daab23aa42a4f8f2adb9a1124c97c9325c
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-28 22:48:32 -0300

    Update main.js

--- pwa-jogo-da-velha/js/main.js
+++ pwa-jogo-da-velha/js/main.js
@@ -4,3 +4,23 @@ window.onload = () => {
         navigator.serviceWorker.register("./sw.js");
     }
 }
+
+
+const QuadradoJogos = document.querySelectorAll(".QuadradoJogo");
+let checarVezJogador = true;
+
+const jogadorX = "X";
+const jogadorO = "O";
+
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".QuadradoJogo")) {
+        jogar(event.target.id);
+    }
+}); 
+
+function jogar(id) {
+    const QuadradoJogo = document.getElementById(id);
+    vezDoJogador = checarVezJogador ? jogadorX : jogadorO;
+    QuadradoJogo.textContent = vezDoJogador;
+    checarVezJogador = !checarVezJogador;
+}
